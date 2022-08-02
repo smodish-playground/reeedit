c505f2052a76f1ad4a26ec84098fc97187c0e85d
Steve Smodish <smodish@hotmail.com>
2022-08-02 12:49:20 -0400

    create form to add a post

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PostForm = () => {
-  return <div>PostForm</div>
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const onTitleChanged = (event: {
+    target: { value: React.SetStateAction<string> }
+  }) => setTitle(event.target.value)
+  const onContentChanged = (event: {
+    target: { value: React.SetStateAction<string> }
+  }) => setContent(event.target.value)
+
+  return (
+    <section>
+      <h2>Add a new post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          name="postTitle"
+          id="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button">Save Post</button>
+      </form>
+    </section>
+  )
 }
 
 export default PostForm
