23027daf95d1902e136c865b858f9289056b8368
Steve Smodish <smodish@hotmail.com>
2022-08-08 12:25:17 -0400

    improve mocked db

--- src/mockdb/db.js
+++ src/mockdb/db.js
@@ -1,20 +1,20 @@
 const data = {
   posts: [
     {
-      id: 'theFirstPost',
+      id: 'jXI_oh1UDHM7If-zDvytk',
       title: "This isn't really even a post",
       topics: ['posting', 'faking it', 'other stuff'],
       content: 'We really should talk about posting',
       createdBy: 'firsty',
-      createdAt: 1690023145,
+      createdAt: 1659975419664,
     },
     {
-      id: 'theSecondPost',
+      id: 'QPichBrG9QGBu3RAc7ipx',
       title: "This isn't really even a post either",
       topics: ['faking it', 'other stuff', 'copying'],
       content: 'We really should talk about posting and commenting',
       createdBy: 'secondly',
-      createdAt: 1691023145,
+      createdAt: 1659975619664,
     },
   ],
   topics: ['copying', 'faking it', 'other stuff', 'posting'],
